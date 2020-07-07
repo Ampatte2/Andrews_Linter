c2de64a245b2009df69402ea2004991a32bfc112
Ampatte2 <ampatte2@asu.edu>
2020-07-07 08:02:50 -0700

    First Commit For Linter

---
+++ index.js
@@ -0,0 +1,24 @@
+const hello = "Hello";
+
+console.log(hello);
+
+let why = "Hello World";
+function thisGuy () {
+    console.log(why);
+}
+thisGuy();
+console.log(why);
+let thisobj = {
+    "foo"             : "bar",
+    "barrious"        : "fooius",
+    "whatchamacallit" : "notcha"
+};
+console.log(thisobj.foo);
+
+const dogootron500 = "ultimate pupper";
+const catdog       = "best of both worlds";
+let helo           = "dogs";
+console.log(catdog, helo, dogootron500);
+if (why === thisobj.foo) {
+    console.log(why);
+}
